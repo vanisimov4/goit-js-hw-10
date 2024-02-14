a9b68bdb9d54075725e5da4bc61dc4234c788494
vanisimov4 <144102084+vanisimov4@users.noreply.github.com>
2024-02-14 22:42:52 +0200

    task2Html

--- src/js/02-snackbar.js
+++ src/js/02-snackbar.js
@@ -1,50 +1,53 @@
-const STORAGE_KEY = 'feedback-form-state';
 
-const form = document.querySelector('form.feedback-form');
 
-form.addEventListener('input', handleFormInput);
-form.addEventListener('submit', handleFormSubmit);
 
-function handleFormInput() {
-  const email = form.elements.email.value.trim();
-  const message = form.elements.message.value.trim();
-  const objForm = {
-    email,
-    message,
-  };
-  saveToLS(STORAGE_KEY, objForm);
-}
+// const STORAGE_KEY = 'feedback-form-state';
 
-function handleFormSubmit(event) {
-  event.preventDefault();
+// const form = document.querySelector('form.feedback-form');
 
-  if (form.elements.email.value === '' || form.elements.message.value === '') {
-    return alert('All form fields must be filled in');
-  }
-  const data = loadFromLS(STORAGE_KEY);
-  console.log(data);
-  localStorage.removeItem(STORAGE_KEY);
-  form.reset();
-}
+// form.addEventListener('input', handleFormInput);
+// form.addEventListener('submit', handleFormSubmit);
 
-function saveToLS(key, value) {
-  const zip = JSON.stringify(value);
-  localStorage.setItem(key, zip);
-}
+// function handleFormInput() {
+//   const email = form.elements.email.value.trim();
+//   const message = form.elements.message.value.trim();
+//   const objForm = {
+//     email,
+//     message,
+//   };
+//   saveToLS(STORAGE_KEY, objForm);
+// }
 
-function loadFromLS(key) {
-  const zip = localStorage.getItem(key);
-  try {
-    return JSON.parse(zip);
-  } catch {
-    return zip;
-  }
-}
+// function handleFormSubmit(event) {
+//   event.preventDefault();
 
-function init() {
-  const data = loadFromLS(STORAGE_KEY) || {};
-  form.elements.email.value = data.email || '';
-  form.elements.message.value = data.message || '';
-}
+//   if (form.elements.email.value === '' || form.elements.message.value === '') {
+//     return alert('All form fields must be filled in');
+//   }
+//   const data = loadFromLS(STORAGE_KEY);
+//   console.log(data);
+//   localStorage.removeItem(STORAGE_KEY);
+//   form.reset();
+// }
 
-init();
+// function saveToLS(key, value) {
+//   const zip = JSON.stringify(value);
+//   localStorage.setItem(key, zip);
+// }
+
+// function loadFromLS(key) {
+//   const zip = localStorage.getItem(key);
+//   try {
+//     return JSON.parse(zip);
+//   } catch {
+//     return zip;
+//   }
+// }
+
+// function init() {
+//   const data = loadFromLS(STORAGE_KEY) || {};
+//   form.elements.email.value = data.email || '';
+//   form.elements.message.value = data.message || '';
+// }
+
+// init();
